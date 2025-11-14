33e31d663d67f589ac34cf6b2a8fb28aff5ab2e0
Diego Hernandez <dieherram@gmail.com>
2025-11-14 12:16:26 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+/*!
+* Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+*/
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Función para reducir la barra de navegación
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Reducir la barra de navegación
+    navbarShrink();
+
+    // Reducir la barra de navegación cuando se desplaza la página
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activar Bootstrap scrollspy en el elemento de navegación principal
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Cerrar barra de navegación receptiva cuando el conmutador es visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Activar plugin SimpleLightbox para elementos del portafolio
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box:not(.external-link)'
+    });
+});
